7bc84b6884551034ab8bd13c9d48897e787e697f
edgarchaves <edjchg@gmail.com>
2018-03-20 14:28:02 -0600

    Prueba jaja

---
+++ Prueba1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Edgar Chaves on 20/3/18.
+//
+
+#include "Prueba1.h"
---
+++ Prueba1.h
@@ -0,0 +1,14 @@
+//
+// Created by Edgar Chaves on 20/3/18.
+//
+
+#ifndef TAREACORTA_PRUEBA1_H
+#define TAREACORTA_PRUEBA1_H
+
+
+class Prueba1 {
+
+};
+
+
+#endif //TAREACORTA_PRUEBA1_H
